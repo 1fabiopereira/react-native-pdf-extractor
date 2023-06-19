04870ad69474b75aaefd3a9d186967427dcc51de
Fábio Pereira <fabio.pereira.gti@gmail.com>
2023-06-19 17:20:47 -0300

    feat: create extractor component

---
+++ src/extractors/core/Styles.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  Container: {
+    backgroundColor: 'white',
+    borderRadius: 4,
+    height: 155,
+    padding: 15,
+  },
+
+  Title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+
+  Row: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+
+  Button: {
+    padding: 16,
+    textAlign: 'center',
+  },
+
+  Text: {
+    color: 'gray',
+  },
+});
