c7b11c3a5a263a395fdc00fbe094d39294c14cd7
Fábio Pereira <fabio.pereira@neon.com.br>
2023-08-16 11:43:19 -0300

    feat: add new pattern (DARF)

--- src/patterns/index.ts
+++ src/patterns/index.ts
@@ -7,6 +7,7 @@ const Brazil = {
     /(\d{5})\.(\d{5})\s(\d{5})\.(\d{6})\s(\d{5})\.(\d{6})\s(\d)\s(\d{14})/, // Banking - Typeable line
     /(\d{12})\s(\d{12})\s(\d{12})\s(\d{12})/, // Tax revenues - Bar code
     /(\d{11})-(\d)\s(\d{11})-(\d)\s(\d{11})-(\d)\s(\d{11})-(\d)/, // Tax revenues - Typeable line
+    /(\d{11})\s(\d{1})\s(\d{11})\s(\d{1})\s(\d{11})\s(\d{1})\s(\d{11})\s(\d{1})/, // Darf
   ],
 };
 
